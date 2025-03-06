4d2a2092fa15e5d7b6a82473e739f421480883cd
naillegal <quluzade.nail@gmail.com>
2025-03-06 23:00:57 +0400

    products and product-detail done

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// home scripts 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 5,
   spaceBetween: 20,
@@ -35,7 +36,9 @@ var swiper = new Swiper(".mySwiper", {
 document.addEventListener("DOMContentLoaded", function () {
   const menuIcon = document.querySelector(".responsive-navbar .fa-bars");
   const responsiveMenu = document.querySelector(".responsive-header-container");
-  const closeButton = document.querySelector(".close-responsive-header-container button");
+  const closeButton = document.querySelector(
+    ".close-responsive-header-container button"
+  );
   const overlay = document.getElementById("overlay");
 
   function openMenu() {
@@ -47,14 +50,13 @@ document.addEventListener("DOMContentLoaded", function () {
   function closeMenu() {
     responsiveMenu.style.left = "-100%";
     overlay.style.display = "none";
-    document.body.style.overflow = ""; 
+    document.body.style.overflow = "";
   }
 
   menuIcon.addEventListener("click", openMenu);
   closeButton.addEventListener("click", closeMenu);
   overlay.addEventListener("click", closeMenu);
 });
-
 
 var adSwiper = new Swiper(".adSwiper", {
   slidesPerView: 1,
@@ -69,6 +71,91 @@ var adSwiper = new Swiper(".adSwiper", {
   },
   pagination: {
     el: "#advertisement-slider .swiper-pagination",
-    clickable: true, 
+    clickable: true,
   },
+});
+
+
+
+// products scripts 
+document.addEventListener("DOMContentLoaded", function () {
+  const filterByElements = document.querySelectorAll(".filter-by");
+
+  filterByElements.forEach((filterBy) => {
+    const toggleBtn = filterBy.querySelector(".toggle-filter");
+
+    if (toggleBtn) {
+      toggleBtn.addEventListener("click", function (e) {
+        e.stopPropagation();
+        filterBy.classList.toggle("active");
+      });
+    }
+  });
+
+  const regulationDropdown = document.querySelector(
+    ".regulation-products-dropdown"
+  );
+  const regulationItems = document.querySelectorAll(
+    ".regulation-products-content > div"
+  );
+
+  regulationItems.forEach((item) => {
+    item.addEventListener("click", function (e) {
+      e.stopPropagation();
+      if (regulationDropdown.style.display === "block") {
+        regulationDropdown.style.display = "none";
+      } else {
+        regulationDropdown.style.display = "block";
+      }
+    });
+  });
+
+  document.addEventListener("click", function (e) {
+    if (
+      regulationDropdown &&
+      !regulationDropdown.contains(e.target) &&
+      !document.querySelector(".regulation-products-content").contains(e.target)
+    ) {
+      regulationDropdown.style.display = "none";
+    }
+  });
+});
+
+
+
+// product-detail scripts 
+var adSwiper = new Swiper(".productDetailSwiper", {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: "#selected-product-info .swiper-button-next",
+    prevEl: "#selected-product-info .swiper-button-prev",
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: "#selected-product-info .swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const counterValueElem = document.querySelector('.counter .counter-value');
+  const minusButton = document.querySelector('.counter .counter-btn.minus');
+  const plusButton = document.querySelector('.counter .counter-btn.plus');
+
+  minusButton.addEventListener('click', function() {
+    let currentValue = parseInt(counterValueElem.textContent, 10);
+    if (currentValue > 1) {
+      counterValueElem.textContent = currentValue - 1;
+    }
+  });
+
+  plusButton.addEventListener('click', function() {
+    let currentValue = parseInt(counterValueElem.textContent, 10);
+    counterValueElem.textContent = currentValue + 1;
+  });
 });
